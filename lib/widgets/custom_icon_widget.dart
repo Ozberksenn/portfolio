33c0cae8566ca318c7a29a370892b98c3f15340f
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-12 00:40:20 +0300

    onTap paramters for custom icon widget

--- lib/widgets/custom_icon_widget.dart
+++ lib/widgets/custom_icon_widget.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/utils/configuration.dart';
 import 'package:portfolio/widgets/padding_widgets.dart';
 
-Widget custoumIcon({String? iconName, Color? bgColor, String? imageUrl}) {
+Widget custoumIcon(
+    {String? iconName,
+    Color? bgColor,
+    String? imageUrl,
+    void Function()? onTap}) {
   return Column(
     children: [
-      Container(
-        width: 48,
-        height: 48,
-        decoration: BoxDecoration(
-            image: imageUrl != null
-                ? DecorationImage(
-                    fit: BoxFit.cover, image: NetworkImage(imageUrl))
-                : null,
-            color: bgColor ?? Colors.transparent,
-            borderRadius: BorderRadius.circular(12.0)),
+      InkWell(
+        onTap: onTap,
+        child: Container(
+          width: 48,
+          height: 48,
+          decoration: BoxDecoration(
+              image: imageUrl != null
+                  ? DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage(imageUrl))
+                  : null,
+              color: bgColor ?? Colors.transparent,
+              borderRadius: BorderRadius.circular(12.0)),
+        ),
       ),
       paddingVertical8(),
       iconName != null
