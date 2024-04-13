45282d78fee98593bfc94280f25655440b369a10
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-13 18:18:01 +0300

    skill data

---
+++ lib/data/data.dart
@@ -0,0 +1,7 @@
+List<dynamic> skill = [
+  ["Flutter", "React Native", "Swift"],
+  ["java Script", "Type Script", "Dart"],
+  ["Node JS", "React"],
+  ["Html", "Css", "Tailwind"],
+  ["PostgreSQL", "Firebase", "MongoDB"],
+];
--- lib/pages/home/dialogs/about_me_bottom_sheet_widget.dart
+++ lib/pages/home/dialogs/about_me_bottom_sheet_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:portfolio/data/data.dart';
 import 'package:portfolio/widgets/padding_widgets.dart';
 import '../../../utils/configuration.dart';
 import 'bottom_sheet_header_widget.dart';
@@ -27,6 +28,68 @@ Widget aboutMeBottomSheet() {
                 image: DecorationImage(
                     image: NetworkImage(
                         "https://avatars.githubusercontent.com/u/96706849?v=4"))),
+          ),
+          paddingVertical32(),
+          Text("Özberk Şen", style: whiteTitleLarge),
+          Text("Developer", style: whiteBodySmall),
+          paddingVertical32(),
+          SizedBox(
+            width: Get.width / 5,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Experience".tr,
+                    style: whiteBodySmall,
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+                paddingHorizontal16(),
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    "1+ years".tr,
+                    style: whiteBodySmall,
+                  ),
+                )
+              ],
+            ),
+          ),
+          paddingVertical16(),
+          SizedBox(
+            width: Get.width / 5,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Skill".tr,
+                    style: whiteBodySmall,
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+                paddingHorizontal16(),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: List.generate(skill.length, (index) {
+                        List<String> sublist = skill[index];
+                        return Row(
+                            children: List.generate(sublist.length, (subIndex) {
+                          return Text(
+                            "${sublist[subIndex]}  ",
+                            style: whiteBodySmall,
+                          );
+                        }));
+                      })),
+                ),
+              ],
+            ),
           ),
         ],
       ),
