e1f73f0e369a1c4963732aafb0844ca17876d695
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-13 19:37:39 +0300

    popup menu

---
+++ lib/pages/home/widgets/popup_menu_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:portfolio/utils/configuration.dart';
+
+Widget popupMenuIcon() {
+  return PopupMenuButton(
+      color: dark,
+      clipBehavior: Clip.antiAlias,
+      elevation: 5,
+      position: PopupMenuPosition.under,
+      child: Icon(
+        Icons.apple,
+        size: 18,
+        color: white,
+      ),
+      itemBuilder: (BuildContext btn) {
+        return [
+          popupItem(title: "About Me"),
+          popupItem(title: "Contact"),
+          popupItem(title: "Project"),
+        ];
+      });
+}
+
+PopupMenuItem<dynamic> popupItem({required String title}) {
+  return PopupMenuItem(
+      height: 30,
+      child: SizedBox(
+          width: Get.width / 2,
+          child: Text(
+            title,
+            style: whiteBodySmall,
+          )));
+}
