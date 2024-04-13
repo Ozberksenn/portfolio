52b0f80f94cf7492974c585cf92bfe0de074ac7f
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-13 19:39:33 +0300

    headeer widget blurred

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:portfolio/Utils/configuration.dart';
@@ -8,60 +10,66 @@ import 'package:portfolio/widgets/custom_bottom_sheet_widget.dart';
 import 'package:portfolio/widgets/padding_widgets.dart';
 
 Widget header() {
-  return Container(
-    width: Get.width,
-    height: 26,
-    color: blackOpacity,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
+  return ClipRRect(
+    child: BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Container(
+        width: Get.width,
+        height: 28,
+        color: blackOpacity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            paddingHorizontal16(),
-            popupMenuIcon(),
-            paddingHorizontal16(),
-            Text(
-              'Finder',
-              style: whiteTitleSmall,
+            Row(
+              children: [
+                paddingHorizontal16(),
+                popupMenuIcon(),
+                paddingHorizontal16(),
+                Text(
+                  'Finder',
+                  style: whiteTitleSmall,
+                ),
+                paddingHorizontal16(),
+                InkWell(
+                  onTap: () =>
+                      customBottomSheet(children: [aboutMeBottomSheet()]),
+                  child: Text(
+                    'About Me',
+                    style: whiteBodySmall,
+                  ),
+                ),
+                paddingHorizontal16(),
+                Text(
+                  'Contact',
+                  style: whiteBodySmall,
+                ),
+                paddingHorizontal16(),
+                Text(
+                  'Projects',
+                  style: whiteBodySmall,
+                )
+              ],
             ),
-            paddingHorizontal16(),
-            InkWell(
-              onTap: () => customBottomSheet(children: [aboutMeBottomSheet()]),
-              child: Text(
-                'About Me',
-                style: whiteBodySmall,
-              ),
-            ),
-            paddingHorizontal16(),
-            Text(
-              'Contact',
-              style: whiteBodySmall,
-            ),
-            paddingHorizontal16(),
-            Text(
-              'Projects',
-              style: whiteBodySmall,
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "EN",
+                      style: whiteTitleSmall,
+                    )),
+                paddingHorizontal8(),
+                Text(
+                  formatDateTime(DateTime.now()),
+                  style: whiteBodySmall,
+                ),
+                paddingHorizontal8()
+              ],
             )
           ],
         ),
-        Row(
-          children: [
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  "EN",
-                  style: whiteTitleSmall,
-                )),
-            paddingHorizontal8(),
-            Text(
-              formatDateTime(DateTime.now()),
-              style: whiteBodySmall,
-            ),
-            paddingHorizontal8()
-          ],
-        )
-      ],
+      ),
     ),
   );
 }
