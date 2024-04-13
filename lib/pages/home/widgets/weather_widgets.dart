fcc44ba1ebc0ba385b5b9c38171ce7a15ed50c0c
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-13 19:41:43 +0300

    weather widget blurred

--- lib/pages/home/widgets/weather_widgets.dart
+++ lib/pages/home/widgets/weather_widgets.dart
@@ -1,34 +1,42 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:portfolio/widgets/padding_widgets.dart';
 import '../../../utils/configuration.dart';
 
 Widget weather() {
-  return Container(
-    width: Get.width / 7,
-    padding: const EdgeInsets.all(16.0),
-    decoration: BoxDecoration(
-        color: blackOpacity,
-        borderRadius: BorderRadius.circular(16.0),
-        border: Border.all(width: 0.2, color: greyBorder)),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Bursa", style: whiteTitleSmall),
-        paddingVertical8(),
-        Text("7°", style: whiteheadlineSmall),
-        paddingVertical8(),
-        Icon(
-          CupertinoIcons.sun_dust,
-          color: white,
-          size: 24,
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(16.0),
+    child: BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Container(
+        width: Get.width / 7,
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+            color: blackOpacity,
+            borderRadius: BorderRadius.circular(16.0),
+            border: Border.all(width: 0.2, color: greyBorder)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Bursa", style: whiteTitleSmall),
+            paddingVertical8(),
+            Text("7°", style: whiteheadlineSmall),
+            paddingVertical8(),
+            Icon(
+              CupertinoIcons.sun_dust,
+              color: white,
+              size: 24,
+            ),
+            paddingVertical8(),
+            Text(
+              "Clear",
+              style: whiteBodySmall,
+            )
+          ],
         ),
-        paddingVertical8(),
-        Text(
-          "Clear",
-          style: whiteBodySmall,
-        )
-      ],
+      ),
     ),
   );
 }
