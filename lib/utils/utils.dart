50bdb81d30e9b5b2fc9e9e2b736c2eb41837623d
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-14 19:15:59 +0300

    url launcher package installed and launch to url

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 String formatDate(DateTime? value) {
   if (value == null) {
@@ -14,4 +15,21 @@ String formatDateTime(DateTime? value) {
   } else {
     return DateFormat.MMMMd().add_Hm().format(value);
   }
+}
+
+Future<void> launchToUrl(String? url) async {
+  if (url == null) {
+    showErrorDialog("Launch Url Error", "Url is empty");
+    return;
+  }
+
+  if (!await launchUrl(Uri.parse(url))) {
+    showErrorDialog("Launch Url Error", "Could not launch $url");
+  }
+}
+
+showErrorDialog(String title, String message) {
+  // todo buraya alert widgetı ekle.
+  print(title);
+  print(message);
 }
