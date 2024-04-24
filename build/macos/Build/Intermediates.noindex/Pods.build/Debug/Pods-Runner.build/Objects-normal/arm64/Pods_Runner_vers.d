f7cd67ca931a69cd2b88ce17ee491c5bc3d5d6f4
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2024-04-24 23:32:51 +0300

    delete macos

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ozberksen/Desktop/projects/portfolio/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/ozberksen/Desktop/projects/portfolio/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/Objects-normal/arm64/url_launcher_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/ozberksen/Desktop/projects/portfolio/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/module.modulemap \
-  /Users/ozberksen/Desktop/projects/portfolio/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-dummy.m \
-  /Users/ozberksen/Desktop/projects/portfolio/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-prefix.pch
